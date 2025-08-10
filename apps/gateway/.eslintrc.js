0d3f573c9d8dd15a4c481f697b50e41eba8a4a47
ZeroDayZ7 <ZeroDayZ7@proton.me>
2025-08-10 08:03:12 +0200

    docs: clarify understanding of pnpm workspace dependency resolution -add eslint

---
+++ apps/gateway/.eslintrc.js
@@ -0,0 +1,12 @@
+// apps/gateway/.eslintrc.js
+module.exports = {
+  extends: ['../../.eslintrc.js'], // dziedziczy config root
+
+  env: {
+    node: true,
+  },
+
+  rules: {
+    // reguły specyficzne dla backendu Express
+  },
+};
