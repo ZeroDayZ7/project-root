154d96d26fa4360bd726e3f61fe51b1601601db3
ZeroDayZ7 <ZeroDayZ7@proton.me>
2025-08-19 14:15:43 +0200

    add

---
+++ packages/cmdr/templates/microservice-template/src/app.ts
@@ -0,0 +1,34 @@
+import env from './config/env.js';
+import { globalErrorHandler, logger, notFoundHandler, setupCommonMiddleware } from '@zerodayz7/common';
+
+import { requestLoggerDev } from '@zerodayz7/common';
+
+const app = setupCommonMiddleware();
+app.use(
+  requestLoggerDev({
+    logger,
+    isDev: env.NODE_ENV === 'development',
+  }),
+);
+
+
+
+// Obsługa 404
+app.use(
+  notFoundHandler({
+    serviceName: env.NAME,
+    isDev: env.NODE_ENV === 'development',
+    logger,
+  }),
+);
+
+// Obsługa błędów
+app.use(
+  globalErrorHandler({
+    serviceName: env.NAME,
+    isDev: env.NODE_ENV === 'development',
+    logger,
+  }),
+);
+
+export default app;
